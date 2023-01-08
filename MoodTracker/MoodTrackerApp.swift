496ecd18ef027360295605e3bae9d1cc07813f76
Jake Watembach <jakewatembach@gmail.com>
2023-01-07 21:06:55 -0700

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Jake Watembach on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
