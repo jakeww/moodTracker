e3d80ebded8741565a1b0a261aa1ecb0b6528df1
Jake Watembach <jakewatembach@gmail.com>
2023-01-09 00:01:55 -0700

    rehaul

--- MoodTracker/MoodTracker.swift
+++ MoodTracker/MoodTracker.swift
@@ -6,61 +6,92 @@
 //
 
 import SwiftUI
+import Foundation
 
-struct MoodTracker: View {
-    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    let moodIcons = ["😀", "😐", "😔", "😠", "😴"]  // you can customize these to match your app's moods
-    var moods: [Int: Int] = [:]  // this dictionary will store the moods and dates
+struct MoodTracker {
+    
+    // MARK: - Properties
+    
+    let calendar = Calendar.current
+    let today: Date
+    let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    var moods: [String: Int] = [:]
+    
+    // MARK: - Initialization
+    
+    init() {
+        today = Date()
+    }
+    
+    // MARK: - Methods
+    
+    mutating func setMood(_ mood: Int, forDay day: Date) {
+        let dayString = weekdays[calendar.component(.weekday, from: day) - 1]
+        moods[dayString] = mood
+        saveMoods()
+    }
+    
+    func getMood(forDay day: Date) -> Int? {
+        let dayString = weekdays[calendar.component(.weekday, from: day) - 1]
+        return moods[dayString]
+    }
+    
+    func saveMoods() {
+        // Save moods to user's profile
+    }
+}
 
+
+struct MoodTrackerView: View {
+    
+    @State private var moodTracker = MoodTracker()
+ // Create instance of MoodTracker
+    @State private var selectedMood: Int?
+    
     var body: some View {
         VStack {
-            Text("Mood Tracker")
-                .font(.title)
-            ScrollView(.horizontal, showsIndicators: false) {
+            // Grid of 7 days
+            ForEach(0..<7, id: \.self) { index in
                 HStack {
-                    ForEach(0..<7, id: \.self) { day in
-                        Button(action: {
-                            // update the moods dictionary with the current mood and date
-                            let calendar = Calendar.current
-                            let currentDate = Date()
-                            let date = calendar.date(byAdding: .day, value: day, to: currentDate)!
+                    Button(action: {
+                        // Set mood for current day
+                        let currentDay = self.moodTracker.today.advanced(by: TimeInterval(86400 * index))
 
-                            let formatter = DateFormatter()
-                            formatter.dateFormat = "yyyy-MM-dd"
-                            let dateString = formatter.string(from: date)
-
-                            self.moods[day] = 0  // you can replace 0 with the current mood index
-                        },
-                        label: {
-                            // show the mood icon if it exists, or a placeholder if it doesn't
-                            if let mood = self.moods[day] {
-                                Text(self.moodIcons[mood])
-                                    .font(.system(size: 50.0))
-                                    .padding(.all, 20.0)  // remove the return keyword here
-                            } else {
-                                Text(" ")
-                                    .padding()  // remove the return keyword here
-                            }
-                        })
-
-
-                        .background(Color.white)
-                        .foregroundColor(Color.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 2)
-                        )
+                        if let mood = self.selectedMood {
+                            self.moodTracker.setMood(mood, forDay: currentDay)  // Call setMood(_:forDay:) on moodTracker instance
+                        }
+                    }) {
+                        Text("\(self.moodTracker.weekdays[index])")
+                            .foregroundColor(.primary)
+                    }
+                    
+                    Spacer()
+                    
+                    // Display mood for current day
+                    if let mood = self.moodTracker.getMood(forDay: self.moodTracker.today.advanced(by: TimeInterval(86400 * index))) {
+ // Call getMood(forDay:) on moodTracker instance
+                        Text("\(mood)")
+                            .foregroundColor(.secondary)
                     }
                 }
-                .padding([.leading, .bottom, .trailing])
             }
-        }.padding(.vertical).background(Color.gray)
+            
+            // Mood selection
+            Picker(selection: $selectedMood, label: Text("Mood")) {
+                ForEach(1..<6, id: \.self) { index in
+                    Text("\(index)").tag(index)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+        .padding()
     }
 }
 
 
-struct MoodTracker_Previews: PreviewProvider {
+struct MoodTrackerView_Previews: PreviewProvider {
     static var previews: some View {
-        MoodTracker()
+        MoodTrackerView()
     }
 }
+
